a5781578f361cbe6e8ad1eb4306125b2ac1cdc34
Johannes Schneider <johannes.schneider@htwg-konstanz.de>
2025-09-12 13:08:08 +0200

    generated on https://start.spring.io/ with Spring Web and Spring Data JPA added

---
+++ src/main/java/de/htwg/in/schneider/saitenweise/backend/SaitenweiseBackendApplication.java
@@ -0,0 +1,13 @@
+package de.htwg.in.schneider.saitenweise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaitenweiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaitenweiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwg/in/schneider/saitenweise/backend/SaitenweiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwg.in.schneider.saitenweise.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaitenweiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
